82a021125fd5de4b9a00087f345d28ff563d36bf
tutich <pkoinange02@gmail.com>
2023-10-31 11:58:39 +0300

    button

--- r1/src/components/Form.jsx
+++ r1/src/components/Form.jsx
@@ -1,16 +1,21 @@
 
 function Form() {
     return(
-        <div>
-            <label htmlFor='name'>CARDHOLDER NAME</label>
-            <input type='text' placeholder='e.g. Jane Appleseed'/>
-            <label htmlFor='name'>CARD NUMBER</label>
-            <input type='text' placeholder='e.g. 1234 5678 9123 0000'/>
-            <label htmlFor='name'>EXP. DATE (MM/YY )</label>
-            <input type='text' placeholder='MM'/>
-            <input type='text' placeholder='YY'/>
-            <label htmlFor='name'>cvc</label>
-            <input type='text' placeholder='e.g. 123'/>
+        <div class="form">
+            <div>
+                <label htmlFor='name'>CARDHOLDER NAME</label>
+                <input type='text' placeholder='e.g. Jane Appleseed'/>
+                <label htmlFor='name'>CARD NUMBER</label>
+                <input type='text' placeholder='e.g. 1234 5678 9123 0000'/>
+                <label htmlFor='name'>EXP. DATE (MM/YY )</label>
+                <input type='text' placeholder='MM'/>
+                <input type='text' placeholder='YY'/>
+                <label htmlFor='name'>cvc</label>
+                <input type='text' placeholder='e.g. 123'/>
+            </div>
+            <div>
+                <button>Confirm</button>
+            </div>
         </div>
     )
 }
