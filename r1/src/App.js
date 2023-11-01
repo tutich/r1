9fde0cc1eeef528db4027179272e802f9824da36
tutich <pkoinange02@gmail.com>
2023-11-01 23:56:19 +0300

    use state react

--- r1/src/App.js
+++ r1/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 import Display from './components/Display';
 import Form from './components/Form';
 
 
 function App() {
+ const [name, setName] = useState("");
+ const [number, setNumber] = useState("");
+
   return (
     <div className="cont">
       <div className="container">
