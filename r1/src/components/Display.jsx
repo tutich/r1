f839ec075212e40b7febce7216ab67e0eb2d6e9d
tutich <pkoinange02@gmail.com>
2023-11-02 18:01:48 +0300

    image styling

--- r1/src/components/Display.jsx
+++ r1/src/components/Display.jsx
@@ -1,11 +1,22 @@
 import bgFront from '../components/images/bg-card-front.png';
 import bgBack from '../components/images/bg-card-back.png';
-
+import cardLogo from '../components/images/card-logo.svg';
 
 function Display() {
     return(
         <div className="img">
-            <img src={bgFront} alt={bgFront} className="front"/>
+            <article className='img-container'>
+                <img src={bgFront} alt={bgFront} className="front"/>
+                <img src={cardLogo} alt='card' className='logo'/>
+                <div className='content'>
+                    <h2 className="numb">1234 5678 9012 3456</h2>
+                    <ul className=''>
+                        <li>Peter Koinange</li>
+                        <>00/00</>
+                    </ul>
+                </div>
+
+            </article>
             <img src={bgBack} alt={bgBack} className="back"/>
         </div>
 
